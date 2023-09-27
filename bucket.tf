931a40a9b37f9597252233895f30d299a87cffaf
= <george.bentes@gmail.com>
2023-09-27 15:44:37 -0300

    fix: bucket and cloudfront relation

---
+++ bucket.tf
@@ -0,0 +1,34 @@
+resource "aws_s3_bucket" "rickys_website" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_policy" "rickys_website_policy" {
+  depends_on = [ aws_cloudfront_distribution.s3_distribution ]
+  bucket = aws_s3_bucket.rickys_website.id
+  policy = data.aws_iam_policy_document.rickys_website_policy_document.json
+}
+
+data "aws_iam_policy_document" "rickys_website_policy_document" {
+  statement {
+
+    sid = "AllowCloudFrontServicePrincipal"
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+    
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.rickys_website.arn}/*"
+    ]
+
+    condition {
+      test = "StringEquals"
+      variable = "AWS:SourceArn"
+      values = [aws_cloudfront_distribution.s3_distribution.arn]
+    }
+  }
+}
