8fa2e89ad1987b4fa176eb3d6e8d0c7ddfe12fb9
= <george.bentes@gmail.com>
2023-09-28 16:07:29 -0300

    add: build and deploy stages

--- main.tf
+++ main.tf
@@ -13,7 +13,25 @@ variable "app_name" {
   description = "Name of the Heroku app provisioned as an example"
 }
 
-resource "heroku_app" "bennun-app-terraform" {
+resource "heroku_app" "bennun_app_terraform" {
   name   = var.app_name
   region = "us"
+}
+
+resource "heroku_build" "bennun_app_build" {
+  app_id = heroku_app.bennun_app_terraform.id
+
+  source {
+    # A local directory, changing its contents will
+    # force a new build during `terraform apply`
+    path = "./public"
+  }
+}
+
+resource "heroku_formation" "bennun_formation" {
+  app_id     = heroku_app.bennun_app_terraform.id
+  type       = "web"
+  quantity   = 1
+  size       = "Standard-1x"
+  depends_on = ["heroku_build.bennun_app_build"]
 }
