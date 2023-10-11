f0b5d131f670ea3d348cacda4708f248e0d86f67
= <george.bentes@gmail.com>
2023-10-10 22:12:43 -0300

    add: creating resource ACM

--- acm/main.tf
+++ acm/main.tf
@@ -19,3 +19,12 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_acm_certificate" "cert" {
+  domain_name         = var.domain_name
+  validation_method   = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
