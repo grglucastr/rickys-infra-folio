a46a6c13330c30a116f92c558f6cb50658ed35bd
= <george.bentes@gmail.com>
2023-10-10 22:27:10 -0300

    del: move value to locals

--- site_infra/variables.tf
+++ site_infra/variables.tf
@@ -1,7 +1,3 @@
-variable "bucket_name" {
-  default = "bennun-labs-rickys-website"
-}
-
 variable "certificate_arn" {
   default = "arn:aws:acm:us-east-1:947434607925:certificate/c19ccf97-5da2-4a94-80e5-9d2296f376f4"
 }
