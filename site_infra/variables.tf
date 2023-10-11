4cde19987465e0cb6e5f1337341179b9bdbb8422
= <george.bentes@gmail.com>
2023-10-10 23:22:52 -0300

    change: certification ARN

--- site_infra/variables.tf
+++ site_infra/variables.tf
@@ -1,5 +1,5 @@
 variable "certificate_arn" {
-  default = "arn:aws:acm:us-east-1:947434607925:certificate/c19ccf97-5da2-4a94-80e5-9d2296f376f4"
+  default = "arn:aws:acm:us-east-1:947434607925:certificate/5b97ded7-6c0f-43da-a13d-e1bbbe600d55"
 }
 
 variable "cloudfront_comment" {
