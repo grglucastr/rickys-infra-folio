12ffc96a2facb41d9e88bea7ead54494c05631e8
= <george.bentes@gmail.com>
2023-10-11 22:08:15 -0300

    add: domains to variables

--- acm/main.tf
+++ acm/main.tf
@@ -24,6 +24,8 @@ resource "aws_acm_certificate" "cert" {
   domain_name         = var.domain_name
   validation_method   = "DNS"
 
+  subject_alternative_names = var.domain_alternative_names
+
   lifecycle {
     create_before_destroy = true
   }
--- acm/variables.tf
+++ acm/variables.tf
@@ -1,3 +1,8 @@
 variable "domain_name" {
-  default = "rickys-data.today"
+  default = "*.rickys-data.today"
+}
+
+variable "domain_alternative_names" {
+  type = list(string)
+  default = ["www.rickys-data.today","rickys-data.today"]
 }
