ab9f77cab54e86f2f17fc28cf86af6fc7d8369bd
= <george.bentes@gmail.com>
2023-10-11 22:08:28 -0300

    add: string values to variables

--- site_infra/variables.tf
+++ site_infra/variables.tf
@@ -1,7 +1,32 @@
-variable "certificate_arn" {
-  default = "arn:aws:acm:us-east-1:947434607925:certificate/5b97ded7-6c0f-43da-a13d-e1bbbe600d55"
-}
-
 variable "cloudfront_comment" {
   default = "Rickys Website CloudFront"
+}
+
+variable "cloudfront_default_root_object" {
+  default = "index.html"
+}
+
+variable "cloudfront_aliases" {
+  type    = list(string)
+  default = [ "*.rickys-data.today" ]
+}
+
+variable "cloudfront_viewer_protocol_policy" {
+  default = "redirect-to-https"
+}
+
+variable "cloudfront_price_class" {
+  default = "PriceClass_All"
+}
+
+variable "CLOUDFRONT_CERTIFICATE_ARN" {
+  description = "Certificate ARN comes from Enviroment variable"
+}
+
+variable "cloudfront_ssl_support_method" {
+  default = "sni-only"
+}
+
+variable "cloudfront_minimum_protocol_version" {
+  default = "TLSv1.2_2021"
 }
